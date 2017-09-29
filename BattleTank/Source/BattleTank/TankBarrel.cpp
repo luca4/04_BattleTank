03a6aee78f7bce4df26936699c612bee27e5e116
luca4 <luca.riva.103@gmail.com>
2017-09-29 11:38:48 +0200

    Clamped barrel elevation

--- BattleTank/Source/BattleTank/TankBarrel.cpp
+++ BattleTank/Source/BattleTank/TankBarrel.cpp
@@ -1,11 +1,17 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "TankBarrel.h"
+#include "Engine/World.h"
 
 
 void UTankBarrel::Elevate(float RelativeSpeed)
 {
+	RelativeSpeed = FMath::Clamp<float>(RelativeSpeed, -1.f, 1.f);
+	float ElevationChange = RelativeSpeed * MaxDegreesPerSecond * GetWorld()->DeltaTimeSeconds;
+	float RawNewElevation = RelativeRotation.Pitch + ElevationChange;
+	float Elevation = FMath::Clamp<float>(RawNewElevation, MinElevationDegrees, MaxElevationDegrees);
 	
+	SetRelativeRotation(FRotator(Elevation, 0, 0));
 }
 
 
